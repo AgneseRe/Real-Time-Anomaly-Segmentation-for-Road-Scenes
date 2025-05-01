23af7c3644ffd0ae6e0fcb7796579aa84f775f9b
AGNESE RE S325676 <s325676@studenti.polito.it>
2025-05-01 11:36:12 +0200

    fix: remove ToTensor custom implementation

--- train/utils/augmentations.py
+++ train/utils/augmentations.py
@@ -185,25 +185,6 @@ class ColorJitter(object):
             74 + (i - 74) * rate for i in range(256)
         ]).clip(0, 255).astype(np.uint8)
         return table[im]
-    
-class ToTensor(object):
-    '''
-    mean and std should be of the channel order 'bgr'
-    '''
-    def __init__(self, mean=(0, 0, 0), std=(1., 1., 1.)):
-        self.mean = mean
-        self.std = std
-
-    def __call__(self, im, lb):
-        im = im.transpose(2, 0, 1).astype(np.float32)
-        im = torch.from_numpy(im).div_(255)
-        dtype, device = im.dtype, im.device
-        mean = torch.as_tensor(self.mean, dtype=dtype, device=device)[:, None, None]
-        std = torch.as_tensor(self.std, dtype=dtype, device=device)[:, None, None]
-        im = im.sub_(mean).div_(std).clone()
-        if not lb is None:
-            lb = torch.from_numpy(lb.astype(np.int64).copy()).clone()
-        return im, lb
 
 class Compose(object):
 
